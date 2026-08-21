b84d7655d236a6ba5706b19dfad725326b853ece
Shangar Arivazhagan (Keys) <62933475+shangar-t-a@users.noreply.github.com>
2026-08-21 09:12:18 +0530

    Release v2 Deep Agent Services and Security Updates (#100)
    Signed-off-by: Shangar Arivazhagan (Keys) <shangar202000@gmail.com>

--- services/bella-chat-service/app/settings/base.py
+++ services/bella-chat-service/app/settings/base.py
@@ -36,7 +36,7 @@ class BellaChatBaseSettings(BaseSettings):
 
     # App Settings
     APP_NAME: str = "Bella"
-    APP_VERSION: str = "1.0.1"
+    APP_VERSION: str = "1.2.0"
     HOST: str = "0.0.0.0"
     PORT: int = 5000
     DEBUG: bool = False
